d5dc387ef6de2ccc6e4e874f5466804a01986ff8
fuentesloic <fuentes.loic@gmail.com>
2017-05-25 19:05:27 -0400

    change menu lang according lang

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -4,15 +4,28 @@ import { NavLink } from 'react-router-dom'
 
 class Nav extends React.Component {
   render() {
-    return (
-      <ul>
-        <li><NavLink to='informations'>A Propos</NavLink></li>
-        <li><NavLink to='acheter'>Acherter un arbre</NavLink></li>
-        <li><NavLink to='conseils'>Conseils</NavLink></li>
-        <li><NavLink to='recuperer-son-arbre'>Récupérer son arbre</NavLink></li>
-        <li><NavLink to='contact'>Nous contacter</NavLink></li>
-      </ul>
-    )
+    let lang = document.location.href
+    if (lang.includes('/fr/')) {
+      return (
+        <ul>
+          <li><NavLink to='informations'>A Propos</NavLink></li>
+          <li><NavLink to='acheter'>Acherter un arbre</NavLink></li>
+          <li><NavLink to='conseils'>Conseils</NavLink></li>
+          <li><NavLink to='recuperer-son-arbre'>Récupérer son arbre</NavLink></li>
+          <li><NavLink to='contact'>Nous contacter</NavLink></li>
+        </ul>
+      )
+    } else {
+      return (
+        <ul>
+          <li><NavLink to='informations'>A Propos</NavLink></li>
+          <li><NavLink to='acheter'>Acherter un arbre</NavLink></li>
+          <li><NavLink to='conseils'>Conseils</NavLink></li>
+          <li><NavLink to='recuperer-son-arbre'>Récupérer son arbre</NavLink></li>
+          <li><NavLink to='contact'>Nous contacter</NavLink></li>
+        </ul>
+      )
+    }
   }
 }
 
